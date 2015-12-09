699cadf09f2fc4bb5da72157bc59d54925da8ad4
Tyler <tyler.rinker@gmail.com>
2015-12-08 20:51:06 -0500

    updated unit tests

--- tests/testthat/test-before.R
+++ tests/testthat/test-before.R
@@ -5,7 +5,8 @@ test_that("before get the paths before a regex",{
     parsed <- list(c("a", "really", "boring", "path", "for", "R"))
     expect_equal(as.character(before(parsed, "^bor")), "a/really")
     expect_equal(as.character(before(parsed, "^bor", include = TRUE)), "a/really/boring")
-    expect_equal(as.character(before(parsed, "^bor", collapse=FALSE)), list(c("a", "really")))
+    expect_equal(before(parsed, "^bor", collapse=FALSE), structure(list(c("a", "really")), class = c("manipulate", "list"
+), parsed = list(c("a", "really"))))
 })
 
 test_that("after gets the paths after a regex",{
@@ -14,6 +15,7 @@ test_that("after gets the paths after a regex",{
 
     expect_equal(as.character(after(parsed, "^bor")), "path/for/R")
     expect_equal(as.character(after(parsed, "^bor", include = TRUE)), "boring/path/for/R")
-    expect_equal(as.character(after(parsed, "^bor", collapse=FALSE)), list(c("path", "for", "R")))
+    expect_equal(after(parsed, "^bor", collapse=FALSE), structure(list(c("path", "for", "R")), class = c("manipulate",
+"list"), parsed = list(c("path", "for", "R"))))
 
 })
